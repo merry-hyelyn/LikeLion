08048c8b52af93f24e17ba8bc84861f38657cf27
merryn <h1335@naver.com>
2019-09-12 22:48:08 +0900

    python prctice

---
+++ Python Practice/Practice_if_1.py
@@ -0,0 +1,11 @@
+# 제어문 - if문 실습_1
+# 점수를 입력받고 조건에 따라 출력
+# 85점 이상 이면 pass
+# 아니면 fail
+
+score = int(input("점수를 입력하세요 : "))
+
+if score >= 85:
+    print("Pass")
+else:
+    print("Fail")
---
+++ Python Practice/Practice_if_2.py
@@ -0,0 +1,10 @@
+# 제어문 - if문 실습_2
+
+activity = input("너 동아리 뭐헤?")
+# 문자열 입력받기 
+
+if activity == "멋쟁이 사자처럼":
+    print("어 너도 멋사야?")
+
+else:
+    print("..그래..")
